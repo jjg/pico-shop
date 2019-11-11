781f9815e0bccc2305b055d12a19f312b3d452a4
Jason J. Gullickson <mr@jasongullickson.com>
2019-11-11 10:39:55 -0600

    Fixed issue with dereferencing products by id.

--- src/app/cart-contents/cart-contents.component.ts
+++ src/app/cart-contents/cart-contents.component.ts
@@ -10,8 +10,7 @@ import { ProductService } from '../product.service';
 })
 export class CartContentsComponent implements OnInit {
 
-  products: Product[];
-  productIds: number[] = [];
+  products: Product[] = [];
   
   constructor(
     private cartService: CartService,
@@ -22,32 +21,16 @@ export class CartContentsComponent implements OnInit {
     this.getProducts();
   }
   
-  getProducts(): void {
-    
-    // DEBUG
-    console.log(this.cartService.products.length);
-    console.log(this.productService);
-    
-    // DEBUG: just use the ID's until we can figure out calling
-    // the product service
-    this.productIds = this.cartService.products;
-    
-    /*
-    // NOTE: will this loop work as expected?
-    this.cartService.products.forEach(function(productId){
-    
-      // TODO: this.productService is out-of-scope because
-      // it's not passed to the callback function, hmm...
+  getProducts(): void {    
+    for (let productId of this.cartService.products) {
       this.productService.getProduct(productId)
-        .subscribe(product => this.products.push(product));
-        
-    });
-    */
-    
+        .subscribe(product => this.products.push(product));    
+    }
   }
   
   onEmptyCart(): void {
     this.cartService.empty();
+    this.products = [];
     this.getProducts();
   }
 
